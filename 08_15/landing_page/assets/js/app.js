15991caffc4f49b8251007eb93919ac2f86b955b
GiampaoloResendePereira <giampaolopereira29@gmail.com>
2024-09-03 15:34:41 -0300

    Giampaolo
    commit

--- 08_15/landing_page/assets/js/app.js
+++ 08_15/landing_page/assets/js/app.js
@@ -1,36 +1,13 @@
 // Hamburger menu selections
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("ul");
 
 // scroll-to-top selection
-const scrollUp = document.querySelector("#scroll-up");
-
-// Theme switcher selection
-const checkbox = document.querySelector("#checkbox");
 
 // Hamburger menu functionality
-function toggleMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
 
-hamburger.addEventListener("click", toggleMenu);
+// Theme switcher selection
 
 // Close menu on nav menu clicks
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
-
-const navLinks = document.querySelectorAll("#nav-link");
-navLinks.forEach(link => link.addEventListener("click", closeMenu));
 
 // scroll to top functionality
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
 
 // Theme switcher functionality
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
-});
