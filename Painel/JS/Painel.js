fc09b230f225ffe2b2046fd80a3a449e10ac7ed6
GiampaoloResendePereira <giampaolopereira29@gmail.com>
2024-09-05 16:15:55 -0300

    Painel
    SENAI

--- Painel/JS/Painel.js
+++ Painel/JS/Painel.js
@@ -1,94 +1,164 @@
 'use strict';
 
-//Criando função para atualizar o relogio
+// Função para atualizar o relógio
 function atualizaRelogio() {
-    //Atribuindo a uma variavel o elemento com id relogio
     const relogio = document.getElementById('relogio');
-    
-    //Obtendo data-hora de agora
     const agora = new Date();
-    //console.log(agora);
-    
-    //Obetendo hora, minutos e segundos da hora atual
     const horasMinutosSegundos = agora.toLocaleTimeString('pt-br', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
-    
-    //Adicionando a página o relógio
     relogio.innerHTML = horasMinutosSegundos;
-
-    //Aplicando recursividade para o relógio atualizando a cada 1000 milisegundos(1 segundo)
-    setTimeout(atualizaRelogio,1000);
+    setTimeout(atualizaRelogio, 1000);
 }
 
-atualizaRelogio();
-
-// Criando função para atualizar saudação
+// Função para atualizar a saudação
 function atualizaSaudacao() {
-    // Obtendo data de agora
     const agora = new Date();
-    //console.log(agora.getDay());
-
-    // Obtendo o número da semana iniciando de 0 indo até 6
     const dia = agora.getDay();
-
-    // Criando Array de dias da semana equivalente
     const dias = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'];
-    //console.log(dias[dia]);
-
-    // Obtendo horas em valor inteiro
     const hora = agora.getHours();
-
-    // Declarando variavel de cumprimento
     let cumprimento = '';
 
-    // Verificação da saudação adequada
     if (hora < 6) {
         cumprimento = 'Boa Madrugada';
     } else if (hora < 12) {
         cumprimento = 'Bom Dia';
     } else if (hora < 18) {
-        cumprimento = 'Boa Tarde'
+        cumprimento = 'Boa Tarde';
     } else {
         cumprimento = 'Boa Noite';
     }
 
-    // Selecionando Elemento 
     const saudacao = document.getElementById('saudacao');
-
-    // Adicionando no documento html
     saudacao.innerHTML = `${dias[dia]} - ${cumprimento}`;
-
-    // Chaando função de forma recursiva
-    setTimeout(atualizaSaudacao,1000);
+    setTimeout(atualizaSaudacao, 60000); // Atualiza a saudação a cada minuto
 }
 
-atualizaSaudacao();
+// Função para abreviar a data
+function abreviaData(data) {
+    const hora_aula = new Date(data);
+    return hora_aula.toLocaleTimeString('pt-br', { hour: '2-digit', minute: '2-digit' });
+}
 
-const imagens = [
+// Função para carregar as aulas
+function carregaAulas() {
+    const aulas = [
+        {
+            "id": 1300,
+            "data": "2024-08-29T03:00:00.000Z",
+            "data_hora_inicio": "2024-08-29T21:00:00.000Z",
+            "data_hora_fim": "2024-08-30T01:00:00.000Z",
+            "turma": "EMP-NBM-03",
+            "instrutor": "JOEL HERBERT BARBOSA SILVA",
+            "unidade_curricular": "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
+            "ambiente": "VTRIA-3-SALA-3004"
+        },
+        {
+            "id": 1280,
+            "data": "2024-08-29T03:00:00.000Z",
+            "data_hora_inicio": "2024-08-29T21:00:00.000Z",
+            "data_hora_fim": "2024-08-30T01:00:00.000Z",
+            "turma": "UMO-MBMM-03",
+            "instrutor": "THADEU VASCONCELOS DA SILVA GOMES",
+            "unidade_curricular": "MECÂNICA BÁSICA DE MOTORES DE MOTOCICLETAS (CH: 100.0000)",
+            "ambiente": "VTRIA-EXTER-EXTERNO"
+        },
+        {
+            "id": 1326,
+            "data": "2024-08-29T03:00:00.000Z",
+            "data_hora_inicio": "2024-08-29T21:30:00.000Z",
+            "data_hora_fim": "2024-08-30T01:00:00.000Z",
+            "turma": "HTC-MEC-4-92",
+            "instrutor": "AFONSO DE JESUS CORDEIRO",
+            "unidade_curricular": "DESENVOLVIMENTO DE SISTEMAS DE AUTOMAÇÃO MECÂNICA (CH: 100.0000)",
+            "ambiente": "VTRIA-3-LAB-3003"
+        }
+    ];
+
+    const tabelaAulas = document.getElementById('aulas');
+    let linhasAulas = '';
+
+    for (let aula of aulas) {
+        linhasAulas += `
+            <tr>
+                <td>${abreviaData(aula.data_hora_inicio)}</td>
+                <td>${abreviaData(aula.data_hora_fim)}</td>
+                <td>${aula.turma}</td>
+                <td>${aula.instrutor}</td>
+                <td>${aula.unidade_curricular}</td>
+                <td>${aula.ambiente}</td>
+            </tr>
+        `;
+    }
+
+    tabelaAulas.innerHTML = linhasAulas;
+}
+
+// Função para carregar os anúncios
+function carregaAnuncios() {
+    const anuncios = [
+        {
+            "id": 11,
+            "alternativo": "fun",
+            "caminho": "1724968856063.jpg",
+            "created_at": "2024-08-29T22:00:56.000Z"
+        },
+        {
+            "id": 12,
+            "alternativo": "tes",
+            "caminho": "1724968882685.jpg",
+            "created_at": "2024-08-29T22:01:22.000Z"
+        }
+    ];
+
+    const anunciosDiv = document.getElementById('anuncios');
+    let listaAnuncios = '';
+
+    for (let imagem of anuncios) {
+        listaAnuncios += `<img src='http://10.145.40.222:8080/public/${imagem.caminho}' alt='${imagem.alternativo}' />`;
+    }
+
+    anunciosDiv.innerHTML = listaAnuncios;
+}
+
+// Inicializando funções
+atualizaRelogio();
+atualizaSaudacao();
+carregaAulas();
+carregaAnuncios();
+
+// Exibindo horários das aulas no console
+const aulas = [
     {
-        "id": 11,
-        "alternativo": "fun",
-        "caminho": "1724968856063.jpg",
-        "created_at": "2024-08-29T22:00:56.000Z"
+        "id": 1300,
+        "data": "2024-08-29T03:00:00.000Z",
+        "data_hora_inicio": "2024-08-29T21:00:00.000Z",
+        "data_hora_fim": "2024-08-30T01:00:00.000Z",
+        "turma": "EMP-NBM-03",
+        "instrutor": "JOEL HERBERT BARBOSA SILVA",
+        "unidade_curricular": "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
+        "ambiente": "VTRIA-3-SALA-3004"
     },
     {
-        "id": 12,
-        "alternativo": "tes",
-        "caminho": "1724968882685.jpg",
-        "created_at": "2024-08-29T22:01:22.000Z"
+        "id": 1280,
+        "data": "2024-08-29T03:00:00.000Z",
+        "data_hora_inicio": "2024-08-29T21:00:00.000Z",
+        "data_hora_fim": "2024-08-30T01:00:00.000Z",
+        "turma": "UMO-MBMM-03",
+        "instrutor": "THADEU VASCONCELOS DA SILVA GOMES",
+        "unidade_curricular": "MECÂNICA BÁSICA DE MOTORES DE MOTOCICLETAS (CH: 100.0000)",
+        "ambiente": "VTRIA-EXTER-EXTERNO"
+    },
+    {
+        "id": 1326,
+        "data": "2024-08-29T03:00:00.000Z",
+        "data_hora_inicio": "2024-08-29T21:30:00.000Z",
+        "data_hora_fim": "2024-08-30T01:00:00.000Z",
+        "turma": "HTC-MEC-4-92",
+        "instrutor": "AFONSO DE JESUS CORDEIRO",
+        "unidade_curricular": "DESENVOLVIMENTO DE SISTEMAS DE AUTOMAÇÃO MECÂNICA (CH: 100.0000)",
+        "ambiente": "VTRIA-3-LAB-3003"
     }
-]
+];
 
-const anuncios = document.getElementById('anuncios');
-let listaAnuncios = '';
-
-for (let imagem of imagens) {
-    listaAnuncios += `<img src='http://10.145.40.222:8080/public/${imagem.caminho}' alt='${imagem.alternativo}' />`
-    console.log(imagem.caminho);
+for (let aula of aulas) {
+    console.log(abreviaData(aula.data_hora_inicio));
 }
-
-anuncios.innerHTML = listaAnuncios;
-
-function atualizaTabela(){
-    
-}
-
